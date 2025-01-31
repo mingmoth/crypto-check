e8330bc7144968baf961d991aa3bbe547b09d929
guanmin <guanmin.l@gaszoos.com>
2025-01-31 17:42:23 +0800

    fix: redefine getTicker

--- src/components/DashBoard.tsx
+++ src/components/DashBoard.tsx
@@ -6,24 +6,13 @@ import CurrencyCard from "@/components/CurrencyCard";
 import CurrencyDrawer from "./CurrencyDrawer";
 import { Drawer, DrawerContent, DrawerHeader, DrawerTrigger, DrawerTitle } from "./ui/drawer";
 import type { Currency } from "@/types";
-
-const tickerData = {
-  symbol: '',
-  price: 0,
-  vol: 0,
-  percent: 0.00,
-  chg: 0,
-  high: 0,
-  low: 0,
-  open: 0,
-  time: 0
-}
+import type { Ticker, TickerState } from "@/store/tickerSlice";
 
 export default function DashBoard () {
   const dispatch = useDispatch();
   const currencies: Currency[] = useSelector(selectAllPairs)
-  const getTicker = useSelector(state => (symbol: string) => selectTickerBySymbol(symbol)(state)) || {...tickerData}
-  // const getTicker = useSelector(selectTickerBySymbol(symbol: string))
+  const selectTickerBySymbolSelector = (symbol: string) => (state: { ticker: TickerState }) => selectTickerBySymbol(symbol)(state);
+  const getTicker = (symbol: string) => useSelector(selectTickerBySymbolSelector(symbol));
 
   useEffect(() => {
     currencies.forEach((currency: Currency) => {
@@ -50,7 +39,7 @@ export default function DashBoard () {
   return (
     <div className="p-4 pt-0 grid xs:grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
       {currencies.map((currency: Currency) => {
-        const ticker = getTicker(currency.symbol)
+        const ticker: Ticker = getTicker(currency.symbol)
         return (
           <Drawer key={`${currency.cid}-${currency.symbol}`}>
             <DrawerTrigger>
@@ -63,7 +52,7 @@ export default function DashBoard () {
                 </div>
                 <div>{ticker?.percent || 0}%</div>
                 <div>
-                  {parseFloat(ticker?.chg || 0).toFixed(
+                  {(parseFloat(ticker?.chg) || 0).toFixed(
                     currency.quote === "USDT" ? 3 : 8
                   )}
                 </div>
